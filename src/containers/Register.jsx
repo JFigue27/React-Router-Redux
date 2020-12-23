df0d44ff22ef5d4d2132e8a3c4fcf7eab4675b3e
Jose Jaime Figueroa Barrios <jaime.figueroa@inspiracode.net>
2020-12-22 20:47:24 -0700

    UI Validations

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { registerRequest } from "../actions";
 import { Link } from "react-router-dom";
+import Header from "../components/Header";
 import "../assets/styles/components/Register.scss";
 
 const Register = (props) => {
@@ -26,36 +27,39 @@ const Register = (props) => {
   };
 
   return (
-    <section className="register">
-      <section className="register__container">
-        <h2>Regístrate</h2>
-        <form className="register__container--form" onSubmit={handleSubmit}>
-          <input
-            name="name"
-            className="input"
-            type="text"
-            onChange={handleInput}
-            placeholder="Nombre"
-          />
-          <input
-            name="email"
-            className="input"
-            type="text"
-            onChange={handleInput}
-            placeholder="Correo"
-          />
-          <input
-            name="password"
-            className="input"
-            type="password"
-            onChange={handleInput}
-            placeholder="Contraseña"
-          />
-          <button className="button">Registrarme</button>
-        </form>
-        <Link to="/login">Iniciar sesión</Link>
+    <>
+      <Header isRegister />
+      <section className="register">
+        <section className="register__container">
+          <h2>Regístrate</h2>
+          <form className="register__container--form" onSubmit={handleSubmit}>
+            <input
+              name="name"
+              className="input"
+              type="text"
+              onChange={handleInput}
+              placeholder="Nombre"
+            />
+            <input
+              name="email"
+              className="input"
+              type="text"
+              onChange={handleInput}
+              placeholder="Correo"
+            />
+            <input
+              name="password"
+              className="input"
+              type="password"
+              onChange={handleInput}
+              placeholder="Contraseña"
+            />
+            <button className="button">Registrarme</button>
+          </form>
+          <Link to="/login">Iniciar sesión</Link>
+        </section>
       </section>
-    </section>
+    </>
   );
 };
 
